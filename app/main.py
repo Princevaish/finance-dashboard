fc378a2664104964268140fb66fd8b5f9c0c5a50
Prince vaish <118712855+Princevaish@users.noreply.github.com>
2026-04-07 07:40:40 +0530

    Add new allowed origin for CORS

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://finance-dashboard-ten-puce.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
