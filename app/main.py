596d11c314411d7171fa606af2760a2773943bf9
Prince vaish <118712855+Princevaish@users.noreply.github.com>
2026-04-07 07:53:10 +0530

    Add new origin to CORS middleware

--- app/main.py
+++ app/main.py
@@ -49,7 +49,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://finance-dashboard-ten-puce.vercel.app"],
+    allow_origins=["http://localhost:3000","https://finance-dashboard-ten-puce.vercel.app","https://finance-dashboard-pquhjo6rv-princevaishs-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
